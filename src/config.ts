cb1b66236dca3b2ee9bce0e06869829918d6734b
jaimesoad <jaime.ac1106@gmail.com>
2022-12-27 00:48:22 -0500

    added variables

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,6 @@ config()
 export const USER = process.env.USER
 export const PASSWD = process.env.PASSWD
 export const DBNAME = process.env.DBNAME
+export const HOST = process.env.HOST
 export const PORT = Number(process.env.PORT)
+export const DBPORT = Number(process.env.DBPORT)
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,10 +1,12 @@
 import {createPool} from "mysql2/promise"
-import {USER, PASSWD, DBNAME} from "../config";
+import {USER, PASSWD, DBNAME, HOST, DBPORT} from "../config";
 
 const db = createPool({
     user: USER,
     password: PASSWD,
-    database: DBNAME
+    database: DBNAME,
+    host: HOST,
+    port: DBPORT
 })
 
 export default db
